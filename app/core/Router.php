6493ef79d321fcb658cda4eaa9c779e5c860b59a
maxprihodko8 <max.prihodko8@gmail.com>
2017-10-08 18:25:16 +0300

    Added router class for routing of application and modified index file for usage of router

---
+++ app/core/Router.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\core;
+
+class Router
+{
+
+    public function run()
+    {
+
+    }
+}
--- web/index.php
+++ web/index.php
@@ -1,4 +1,7 @@
 <?php
+use app\core\Router;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$router = new Router(__DIR__ . '/app/config/routes.php');
+$router->run();
