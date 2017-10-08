b7aaf64d7e7908c8b84415d4846830082475a1bd
maxprihodko8 <max.prihodko8@gmail.com>
2017-10-08 18:31:02 +0300

    added uri get from request to router

--- app/core/Router.php
+++ app/core/Router.php
@@ -4,9 +4,39 @@ namespace app\core;
 
 class Router
 {
+    private $routes;
+
+    /**
+     * Router constructor.
+     * @param $routesPath
+     * constructor of router of application with router config file path
+     */
+    public function __construct($routesPath) {
+        $this->routes = include ($routesPath);
+    }
+
+    /**
+     * @return string
+     *
+     * Gets uri from request
+     */
+    public function getURI() {
+        if(!empty($_SERVER['REQUEST_URI'])) {
+            return trim($_SERVER['REQUEST_URI'], '/');
+        }
+
+        if(!empty($_SERVER['PATH_INFO'])) {
+            return trim($_SERVER['PATH_INFO'], '/');
+        }
+
+        if(!empty($_SERVER['QUERY_STRING'])) {
+            return trim($_SERVER['QUERY_STRING'], '/');
+        }
+        return '/';
+    }
 
     public function run()
     {
-
+        $this->getURI();
     }
 }
--- web/index.php
+++ web/index.php
@@ -3,5 +3,5 @@ use app\core\Router;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$router = new Router(__DIR__ . '/app/config/routes.php');
+$router = new Router(__DIR__ . '/../app/config/routes.php');
 $router->run();
