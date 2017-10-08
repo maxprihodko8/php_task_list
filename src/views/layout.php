c86d7a6414e9e298472f10d8ab7d01d3d5fb8283
maxprihodko8 <max.prihodko8@gmail.com>
2017-10-08 21:27:23 +0300

    Modified css and added some style

--- src/views/layout.php
+++ src/views/layout.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <title>PHP TASK LIST</title>
     <script type="application/javascript" src="/web/js/angular.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="/web/css/style.css">
 </head>
 <body>
 
--- src/views/site/index.php
+++ src/views/site/index.php
@@ -2,19 +2,17 @@
     <span class="errors_field">
         {{errors}}
     </span>
-    <ul ng-repeat="task in tasks">
-        <li>
-            {{task.id}}
-        </li>
-
-        <li>
-            {{task.status}}
-        </li>
-        <li>
-            {{task.text}}
+    <ul ng-repeat="task in tasks" class="list">
+        <li class="list-item">
+            <div class="list-content">
+                <h2>{{task.status}}</h2>
+                <img ng-src="{{task.image}}" />
+                <p>{{task.text}}</p>
+            </div>
         </li>
     </ul>
 </div>
+
 
 
 <script type="application/javascript" src="/web/js/angular_app.js"></script>
