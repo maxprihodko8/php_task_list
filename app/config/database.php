5acbeebd82474131d2694e821ba916f9de9af49f
maxprihodko8 <max.prihodko8@gmail.com>
2017-10-08 20:13:04 +0300

    added dbname

--- app/config/database.php
+++ app/config/database.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'hostname' => 'mysql:host=localhost',
+    'hostname' => 'mysql:host=localhost;dbname=php_list',
     'username' => 'root',
     'password' => 'root',
     'options' => [
